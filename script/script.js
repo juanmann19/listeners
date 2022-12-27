44b220a05becb9e1700b16e5af82348613c86da3
Juan <juanmanu2350@gmail.com>
2022-12-26 19:07:33 -0600

    Proyecto terminado

--- script/script.js
+++ script/script.js
@@ -41,15 +41,80 @@ agregarMonitores();
 
 //funcion para agregar listeners a los botones
 function agregarMonitores() {
+    document.getElementById('mostrarPaises').addEventListener("click",mostrarPaises);
+    document.getElementById('agregarPais').addEventListener("click",agregarPais);
     //TODO
 }
 
 
 //funcion que mostrara las paises en la pagina
 function mostrarPaises() {
-  //TODO
+  let tablebody = "";
+  for(let pais of paises){
+     tablebody+=
+     `
+     <table class="table table-sm table-success table-striped bg-gradient-warning">
+                <tbody>
+                    <th>
+                        Nombre
+                    </th>
+                    <th>
+                        Capital
+                    </th>
+                    <th>
+                        Idioma
+                    </th>
+                    <th>
+                        Moneda
+                    </th>
+                    <tr>
+                        <td>${pais.nombre}</td>
+                        <td>${pais.capital}</td>
+                        <td>${pais.idioma_oficial}</td>
+                        <td>${pais.moneda}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                    <td colspan="4" class="text-center">
+                        <img src="${pais.bandera}" alt="" srcset="">
+                    </td>
+
+                    </tr>
+                </tfoot>
+                
+            </table>
+     
+     `;
+    }
+    document.getElementById("paises").innerHTML = tablebody;
 }
 
 //funcion que permite agregar un pais al arreglo
 function agregarPais() {
+    let newpais = {
+        nombre: prompt("Ingresa el nombre"),
+        bandera: prompt("Ingresa la bandera"),
+        capital: prompt("Ingresa la capital"),
+        idioma_oficial: prompt("Ingresa el idioma"),
+        moneda: prompt("Ingresa la moneda") 
+    }
+    if(cheq(newpais)){
+        paises.unshift(newpais);
+        mostrarPaises();
+    }
+    else{
+        alert("Uno o mas campos estan vacios, pais no agregado")
+    }
+
 }
+
+function cheq(obj) {
+    for (var key in obj) {
+        if (obj[key] === null || obj[key] == "")
+            return false;
+    }
+    return true;
+}
+
+
